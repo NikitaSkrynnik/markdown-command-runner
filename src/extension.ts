87acf95b3c0b85af7a85f7d9e6e68623c927113b
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-03-30 18:35:26 +0700

    fix for deployments-k8s bash commands
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,27 @@
 import * as vscode from 'vscode';
 import { CommandCodeLensProvider } from './commandCodeLensProvider';
 import cp = require('child_process');
+import { basename } from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('markdown.run.command', (args) => {
-			var term = vscode.window.activeTerminal || vscode.window.createTerminal();
+			var editor = vscode.window.activeTextEditor;
+			var currentFilePath = "";
+			if (editor != undefined) {
+				currentFilePath = editor.document.fileName;
+				currentFilePath = currentFilePath.substring(0, currentFilePath.lastIndexOf('/'))
+			}
+			var term = vscode.window.activeTerminal;
+
+			if (term == undefined) {
+				term = vscode.window.createTerminal();
+				term.sendText("cd " + currentFilePath);
+			}
 			// check if there's a running command in the active terminal, if there is one
 			// create a new term
-			term.processId.then(pid => {
-				cp.exec('ps -o state= -p ' + pid, (error, stdout, stderr) => {
-					if (error) {
-						// if we can't check just send to the current one...
-						term.show();
-						term.sendText(args.command);
-						return;
-					}
-
-					// a + in the state indicates a process running in foreground
-					if (!stdout.includes('+')) {
-						term = vscode.window.createTerminal();
-					}
-					term.show();
-					term.sendText(args.command);
-				});
-			});
+			term.show();
+			term.sendText(args.command);
 		})
 	);
 
